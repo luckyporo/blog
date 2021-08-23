9fe27325727799758d06e80530f96ea987d13fea
luckyporo <565010808@qq.com>
2021-08-23 10:02:27 +0800

    fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,12 +7,12 @@ export default function Footer() {
     <footer>
       <div className="flex flex-col items-center mt-16">
         <div className="flex mb-3 space-x-4">
+          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={5} />
+          <SocialIcon kind="yuque" href={siteMetadata.yuque} size={5} />
           <SocialIcon kind="github" href={siteMetadata.github} size={5} />
           {/* <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} /> */}
-          <SocialIcon kind="yuque" href={siteMetadata.yuque} size={6} />
-          <SocialIcon kind="juejin" href={siteMetadata.juejin} size={5} />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size={5} />
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={5} />
+          {/* <SocialIcon kind="juejin" href={siteMetadata.juejin} size={5} /> */}
         </div>
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
